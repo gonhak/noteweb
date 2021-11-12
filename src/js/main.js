d01aae25417f5c493d390c1ab16dbe7b462e573f
Filip Natanek <natanek291@gmail.com>
2021-11-12 01:59:07 +0100

    new prostotype of navigation and added breakpoints

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,30 @@
+// Variables
+
 const navigation = document.querySelector('.nav__container__page');
 const navBtnCollapse = document.querySelector('.nav__container__button__collapse');
 const navIcon = document.querySelector('.fa-chevron-circle-up');
 
+// Colapsing nav
 
 const navColapse = () => {
     navBtnCollapse.classList.toggle('active');
     
     if(navBtnCollapse.classList.contains('active')){
-        navIcon.style.transform = "rotate(90deg)";
-        navigation.style.right = "0"
+        navIcon.style.transform = "rotate(0deg)";
+        navigation.style.bottom = "-100px";
     }else{
-        navIcon.style.transform = "rotate(-90deg)";
-        navigation.style.right = "-100vw"
+        navIcon.style.transform = "rotate(-180deg)";
+        navigation.style.bottom = "0";
     };
 };
 
 navBtnCollapse.addEventListener('click', navColapse);
+
+
+// ---------------------------------------------------------------------------------------------
+// Date
+
+// const copyRightYear = document.querySelector('.nav__container__page__copyright__year');
+
+// const year = new Date().getFullYear();
+// copyRightYear.innerText = year;
