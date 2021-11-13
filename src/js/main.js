6ec93366980b258672017f5bc06cef6d1f646257
Filip Natanek <natanek291@gmail.com>
2021-11-13 01:22:06 +0100

    the final version of desktop navigation and class cards.To do : searching system by using cards id at the arr

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,56 @@
 // Variables
 
-const navigation = document.querySelector('.nav__container__page');
-const navBtnCollapse = document.querySelector('.nav__container__button__collapse');
+const navigation = document.querySelector('.nav__container__mobile__page');
+const navBtnCollapse = document.querySelector('.nav__container__mobile__button__collapse');
 const navIcon = document.querySelector('.fa-chevron-circle-up');
+const popupQuitBtn = document.querySelector('.main__container__desktop__popup__container__quit');
+const popup = document.querySelector('.main__container__desktop__popup');
+const searchingDesktopBtn = document.querySelector('.nav__container__desktop__menu__button--search');
 
 // Colapsing nav
 
-const navColapse = () => {
+const navColapseMobile = () => {
     navBtnCollapse.classList.toggle('active');
     
     if(navBtnCollapse.classList.contains('active')){
         navIcon.style.transform = "rotate(0deg)";
-        navigation.style.bottom = "-100px";
+        navigation.style.bottom = "-99px";
     }else{
         navIcon.style.transform = "rotate(-180deg)";
         navigation.style.bottom = "0";
     };
 };
 
-navBtnCollapse.addEventListener('click', navColapse);
+// Quiting popup
+
+const quitingPopup = () => {
+    popup.classList.toggle('Disabled');
+    popup.classList.toggle('Active');
+
+    if(popup.classList.contains('Disabled')){
+        popup.style.opacity = "0";
+        popup.style.zIndex = "-10";
+    };
+};
+
+// showing popup
+
+const showingPopup = () => {
+    popup.classList.toggle('Active');
+    popup.classList.remove('Disabled');
+
+    if(popup.classList.contains('Active')){
+        popup.style.opacity = "1";
+        popup.style.zIndex = "5";
+        console.log("dodaje klase active");
+    };
+};
+
+// Listeners
+
+navBtnCollapse.addEventListener('click', navColapseMobile);
+popupQuitBtn.addEventListener('click', quitingPopup);
+searchingDesktopBtn.addEventListener('click', showingPopup);
 
 
 // ---------------------------------------------------------------------------------------------
